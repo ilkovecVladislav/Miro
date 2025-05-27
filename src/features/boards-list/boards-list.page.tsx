bcecacbb8350fd560cdd1c3a76080d5bda63a460
Vlad Ilkovec <ilkovec.vi@gmail.com>
2025-05-27 21:47:35 +0300

    added mocked api

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,7 +1,71 @@
+import { rqClient } from "@/shared/api/instance";
+import { CONFIG } from "@/shared/model/config";
+import { ROUTES } from "@/shared/model/routes";
+import { href, Link } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+
 function BoardsListPage() {
+  const queryClient = useQueryClient();
+  const boardsQuery = rqClient.useQuery("get", "/boards");
+
+  const deleteBoardMutation = rqClient.useMutation(
+    "delete",
+    "/boards/{boardId}",
+    {
+      onSettled: async () => {
+        await queryClient.invalidateQueries(
+          rqClient.queryOptions("get", "/boards"),
+        );
+      },
+    },
+  );
+
+  const createBoardMutation = rqClient.useMutation("post", "/boards", {
+    onSettled: async () => {
+      await queryClient.invalidateQueries(
+        rqClient.queryOptions("get", "/boards"),
+      );
+    },
+  });
+
   return (
     <div>
-      <h1>Boards list</h1>
+      <h1>Boards list {CONFIG.API_BASE_URL}</h1>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target as HTMLFormElement);
+          createBoardMutation.mutate({
+            body: {
+              name: formData.get("name") as string,
+            },
+          });
+        }}
+      >
+        <input name="name" type="text" />
+        <button type="submit" disabled={createBoardMutation.isPending}>
+          Create board
+        </button>
+      </form>
+
+      {boardsQuery.data?.map((board) => (
+        <div key={board.id}>
+          <Link to={href(ROUTES.BOARD, { boardId: board.id })}>
+            {board.name}
+          </Link>
+          <button
+            disabled={deleteBoardMutation.isPending}
+            onClick={() =>
+              deleteBoardMutation.mutate({
+                params: { path: { boardId: board.id } },
+              })
+            }
+          >
+            Delete
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
